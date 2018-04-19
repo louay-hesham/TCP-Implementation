cb18b3a6e3a7b1d20c516aec9a9a6e4947107c64
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2018-04-19 12:58:16 +0200

    " initial connection "

--- server test.py
+++ server test.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python
-
 import socket
-
-
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 2  # Normally 1024, but we want fast response
-   
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
+s.bind(('localhost', 50000))
 s.listen(1)
-
-conn, addr = s.accept()
-print ('Connection address:', addr)
 while 1:
-  data = conn.recv(BUFFER_SIZE)
-  if not data: break
-  print ("received data:", data.decode())
-  # conn.send(data)  # echo
-conn.close()
+    conn, addr = s.accept()
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+    conn.close()
+
