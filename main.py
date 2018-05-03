e55d8808fd5f7f3c25f7be29e4f81a63646dc773
Louay Morsi <louay.hesham@gmail.com>
2018-05-03 20:15:23 +0200

    Created function to generate stats

--- main.py
+++ main.py
@@ -21,14 +21,54 @@ def start_server():
 def start_client():
   return start_new_process(client_command)
 
-# def start_thread(func):
-#   return pool.apply_async(func,(config))
+def get_avg_throughput():
+  start_new_thread(start_server,())
+  time.sleep(1)
+  i = 5
+  throughput = 0
+  while i > 0:
+    throughput += client.client(config)
+    time.sleep(1)
+    i -= 1
+  throughput /= 5;
+  config.TCP_PORT += 1
+  return throughput
 
 def generate_statistics():
-  start_new_thread(start_server,())
-  time.sleep(2)
-  c = client.client(config)
-  for t in [s, c]:
-    print(t)
-  print('done')
+  probabilities = [0.01, 0.05, 0.1, 0.3]
+  window_sizes = [1, 10, 100, 500, 1000]
+  config.TCP_PORT = 50000
+  stats = {}
+  print('Testing Stop-and-Wait algorithm')
+  config.algorithm = 'S&W'
+  stats['S&W'] = {}
+  for p in probabilities:    
+    config.plp = config.pcp = p
+    stats['S&W'][p] = get_avg_throughput()
+
+  print('Testing Selective-Repeat algorithm')
+  config.algorithm = 'SR'
+  stats['SR'] = {}
+  for ws in window_sizes:
+    config.window_size = ws
+    stats['SR'][ws] = {}
+    for p in probabilities:    
+      config.plp = config.pcp = p
+      stats['SR'][ws][p] = get_avg_throughput()
+
+  print('Testing Go-Back-N algorithm')
+  config.algorithm = 'GBN'
+  stats['GBN'] = {}
+  for ws in window_sizes:
+    config.window_size = ws
+    stats['GBN'][ws] = {}
+    for p in probabilities:    
+      config.plp = config.pcp = p
+      stats['GBN'][ws][p] = get_avg_throughput()
+
+  print(stats['S&W'])
+  print(stats['GBN'])
+  print(stats['SR'])
+  return stats
+  
   
