ecd09d1626d850da53b587963a5abc5b209409f1
Louay Morsi <louay.hesham@gmail.com>
2018-05-06 19:48:19 +0200

    Removed some server files to reduce repo size

--- config.py
+++ config.py
@@ -4,19 +4,11 @@ algorithms = ['SR', 'GBN', 'S&W']  # our 3 types of algorithms
 algorithm = algorithms[0]          # selective repeat chosen in this run
 
 files = [                  # files to be transfered
-  '13. Push buttons.mp3',
    'السيف القاطع.mp3',
-   'Sad Romance (Violin Version)  - YouTube.flv',
-   'Running With The Wolves.mp3',
    'pirates_of_the_caribbean_theme.mp3',
-   'Lay It On Me.mp3',
    'Fullmetal Alchemist - Brotherhood OP 3 [720p] [English sub]_-gY4kmxphBU_youtube.mp3',
    'Fullmetal Alchemist - Brotherhood OP 2 [720p] [English sub]_cpRV6jFIvqQ_youtube.mp3',
-   'Coldplay - Hymn For The Weekend (Violin Cover by Robert Mendoza).mp3',
-   'Careless Whisper - Vintage 1930s Jazz Wham! Cover ft. Dave Koz.mp4',
    'Alesso - Heroes (We Could Be) feat. Tove Lo (Official Audio-Lyrics Video).mp3',
-   '02 - Forsaken.mp3',
-   '04 The Unforgiven II.mp3',
    'AWAKEN.mp3'
 ]
 
